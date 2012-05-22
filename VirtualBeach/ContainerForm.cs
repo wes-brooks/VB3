abc46eeef289ea1ead59bf74610197ae85116df3
Tonia Roddick <troddick@USGS.gov>
2012-05-22 10:17:10 -0500

    worked on IPyPLS Prediction and IPy Modeling plugins

--- VirtualBeach/ContainerForm.cs
+++ VirtualBeach/ContainerForm.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.ComponentModel.Composition.Hosting;
 using System.ComponentModel.Composition;
 using DotSpatial.Controls;
 using DotSpatial.Controls.Docking;
-using VBTools;
 
 namespace VirtualBeach
 {
@@ -19,29 +13,37 @@ namespace VirtualBeach
         [Export("Shell", typeof(ContainerControl))]
         private static ContainerControl Shell;
 
-        private VBProjectManager.VBProjectManager projectmanager = new VBProjectManager.VBProjectManager();
-
+        /// <summary>
+        /// Initializes a new instance of the MainForm class.
+        /// </summary>
         public ContainerForm()
         {
             InitializeComponent();
 
-            //Hook up the event handler that will 
-            ProjectSaved += new ContainerForm.ProjectSavedHandler<PackEventArgs>(ProjectSavedListener);
+            //t
+            appManager = new AppManager();
+            //don't need the map and legend
+            //appManager.Map = new Map();
+            //appManager.Legend = new Legend();
 
-            appManager.SatisfyImportsExtensionsActivated +=
-                delegate(object sender, EventArgs e)
-                {
-                    //appManager.HeaderControl.Add(new DotSpatial.Controls.Header.HeaderItem());
-                };
             
-            //Set the main application window to be the "Shell", which is where 
+            //appManager.SatisfyImportsExtensionsActivated +=
+            //    delegate(object sender, EventArgs e)
+            //    {
+            //        // we use this event to ensure that legend and map dockable panels are
+            //        // added to the DockManager before any other dockable panels.
+            //        //appManager.HeaderControl.Add(new DotSpatial.Controls.Header.HeaderItem());
+            //};
+            
+            //Set the main application window to be the "Shell" 
             Shell = this;
             appManager.LoadExtensions();
 
-
+            //Hook up the event handler that will 
+            //ProjectSaved += new ContainerForm.ProjectSavedHandler<PackEventArgs>(ProjectSavedListener);
             
         }
 
-
+        public AppManager appManager { get; set; } 
     }
 }
